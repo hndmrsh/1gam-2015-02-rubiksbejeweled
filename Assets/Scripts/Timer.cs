60aceb2c5e38e3c5b03737fb10de70b2d88f7971
Samuel Hindmarsh <Samuel.Hindmarsh@gmail.com>
2015-02-20 00:19:35 +1300

    Timer and UI logic

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Timer : MonoBehaviour {
+
+    public interface TimerListener
+    {
+        void TimerTick(float remainingTime);
+    }
+
+    private float remainingTime;
+    private bool started;
+
+    private List<TimerListener> listeners;
+
+    void Start()
+    {
+        remainingTime = 0f;
+        started = false;
+    }
+
+    public void StartTimer(int startingTime)
+    {
+        remainingTime = startingTime;
+        started = true;
+    }
+
+    void Update()
+    {
+        if (started)
+        {
+            remainingTime -= Time.deltaTime;
+
+            if (listeners != null && listeners.Count > 0)
+            {
+                foreach (TimerListener l in listeners)
+                {
+                    l.TimerTick(remainingTime);
+                }
+            }
+
+            if (remainingTime <= 0f)
+            {
+                started = false;
+                remainingTime = 0f;
+            }
+        }
+    }
+
+    public void AddTime(float time)
+    {
+        remainingTime += time;
+    }
+
+    public void RegisterListener(TimerListener listener)
+    {
+        if (listeners == null)
+        {
+            listeners = new List<TimerListener>();
+        }
+
+        listeners.Add(listener);
+    }
+
+    public bool UnregisterListener(TimerListener listener)
+    {
+        if (listeners != null)
+        {
+            return listeners.Remove(listener);
+        }
+
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour, Timer.TimerListener {
+
+    public Text secondsDisplay;
+    public Text millisDisplay;
+
+    public float maxTimeFontSizeMultiplier = 1.25f;
+    
+    public int timeBlockLength = 10;
+    public int shortTimeBlockLength = 1;
+    public int shortTimeBlockThreshold = 10;
+
+    public AnimationCurve fontSizeCurve;
+
+    private Timer timer;
+    private int secondsFontSize, millisFontSize;
+
+	// Use this for initialization
+	void Start () 
+    {
+        secondsFontSize = secondsDisplay.fontSize;
+        millisFontSize = millisDisplay.fontSize;
+
+        timer = gameObject.GetComponent<Timer>();
+        timer.RegisterListener(this);
+
+        timer.StartTimer(60);
+	}
+
+    void Timer.TimerListener.TimerTick(float remainingTime)
+    {
+        float time = Mathf.Max(remainingTime, 0f);
+        secondsDisplay.text = ((int)time).ToString("D2");
+
+        int millis = (int)((time % (int)time) * 100);
+        millisDisplay.text = (Mathf.Max(millis, 0).ToString("D2"));
+
+        float timeThisBlock = timeBlockLength - (time % timeBlockLength);
+        float fontSizeMultiplier = Mathf.Lerp(1f, maxTimeFontSizeMultiplier, fontSizeCurve.Evaluate(timeThisBlock));
+
+        Logger.SetValue("timeThisBlock", timeThisBlock.ToString());
+        Logger.SetValue("fontSizeMultiplier", fontSizeMultiplier.ToString());
+
+        secondsDisplay.fontSize = (int) (secondsFontSize * fontSizeMultiplier);
+        millisDisplay.fontSize = (int)(millisFontSize * fontSizeMultiplier);
+
+        if (time < shortTimeBlockThreshold + 1)
+        {
+            timeBlockLength = shortTimeBlockLength;
+        }
+    }
+}
